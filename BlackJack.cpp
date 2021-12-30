1431a733132060bc941b9d2e08fe76fd2dbb1fc1
Alux6 <Alux6mc@gmail.com>
2021-12-30 13:35:28 +0100

    Arreglada la indentación

--- BlackJack.cpp
+++ BlackJack.cpp
@@ -3,84 +3,83 @@
 using namespace std;
 
 class Carta{
-    private:
-        int valor;
-        int cantidad;
-    public:
-        Carta(int Valor){
-            valor = Valor;
-        }
+	private:
+		int valor;
+		int cantidad;
+	public:
+		Carta(int Valor){
+			valor = Valor;
+		}
 
-        int getValor(){
-            return valor;
-        }
-        int getCantidad(){
-            return cantidad;
-        }
+		int getValor(){
+			return valor;
+		}
+		int getCantidad(){
+			return cantidad;
+		}
 
-        void usarCarta(){
-            cantidad--;
-        }
+		void usarCarta(){
+			cantidad--;
+		}
 };
 
 class Jugador{
-    private:
-        int victorias;
-        int valorTotal;
-        bool jugando;
-        bool banca;
-    public:
+	private:
+		int victorias;
+		int valorTotal;
+		bool jugando;
+		bool banca;
+	public:
+		Jugador(){
+			victorias = 0;
+		}
+		Jugador(bool banca){
+			banca = true;
+			victorias = 0;
+		}
+		int getValor(){
+			return valorTotal;
+		}
+		int getVictorias(){
+			return victorias;
+		}
+		bool getJugando(){
+			return jugando;
+		}
 
-        Jugador(){
-            victorias = 0;
-        }
-        Jugador(bool banca){
-            banca = true;
-            victorias = 0;
-        }
-
-        int getValor(){
-            return valorTotal;
-        }
-        int getVictorias(){
-            return victorias;
-        }
-        bool getJugando(){
-            return jugando;
-        }
-
-        void jugar(){
-            jugando = true;
-            valorTotal = 0;
-        }
-        void terminarRonda(){
-            jugando = false;
-        }
-        void sumarValor(int valor){
-            valorTotal += valor;
-        }
-        void sumarVictoria(){
-            victorias++;
-        }
+		void jugar(){
+			jugando = true;
+			valorTotal = 0;
+		}
+		void terminarRonda(){
+			jugando = false;
+		}
+		void sumarValor(int valor){
+			valorTotal += valor;
+		}
+		void sumarVictoria(){
+			victorias++;
+		}
 
 
-};
+		};
 
 class Juego{
-    private:
-        bool terminado;
-        int rondas = 0;
-        int numeroJugadores;
+	private:
+		bool terminado;
+		int rondas = 0;
+		int numeroJugadores;
 
-    public:
-        Juego(){
-            numeroJugadores = 2;
-        }
-        Juego(int numJugadores){
-            numeroJugadores = numJugadores;
-        }
+	public:
+		Juego(){
+			numeroJugadores = 2;
+		}
+		Juego(int numJugadores){
+			numeroJugadores = numJugadores;
+		}
+		void SacarCarta(){
 
-
+		}
 };
 int main(){
 
